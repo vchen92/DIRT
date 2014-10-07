2708dc010d91aec49bbf681796c29f7f7dda26ce
Gord Larson <gordlarson@gmail.com>
2014-10-07 16:44:31 -0700

    setup some basic stuff

---
+++ DIRT.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Main entrance point for DIRT
+"""
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='DIRT.py',
+                                    description='Find reused text in a corpus of text')
+
+    parser.add_argument('input_dir',
+                        help='Directory containing input corpus')
+    parser.add_argument('output_dir',
+                        help='Directory for output files')
+
+    args = parser.parse_args()
