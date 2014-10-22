667dae979b8d7fded3ea39dd82fcc0cc7eab83a6
Gord Larson <gordlarson@gmail.com>
2014-10-21 21:45:24 -0700

    accedental add (+1 squashed commit) Squashed commits: [ef22ba7] add word segmentation test

--- preprocessing/language_standardizer/zhi.py
+++ preprocessing/language_standardizer/zhi.py
@@ -2,6 +2,15 @@ import jieba
 
 
 def standardize_line(line):
-    words = jieba.cut(line)
+    words = segment_words(line)
     # Remove punctuation?
     return ' '.join(words)
+
+def segment_words(unsegmented):
+    """
+    Segment input text into words
+    :param unsegmented: raw input text, words unseparated by spaces
+    :return: generator of words
+    """
+    words = jieba.cut(unsegmented)
+    return words
