4b89206320fb97cec5172cb0c9b56e1f0bd8e286
Gord Larson <gordlarson@gmail.com>
2014-10-23 14:55:39 -0700

    closer to working

--- preprocessing/tei/reader.py
+++ preprocessing/tei/reader.py
@@ -2,24 +2,8 @@ from lxml import etree
 
 import tei_document
 
-TAG_HEADER = 'teiHeader'
-TAG_FILE_DESC = 'fileDesc'
-TAG_TITLE_STATEMENT = 'titleStmt'
-TAG_TITLE = 'title'
-TAG_PRINCIPAL = 'principal'
-TAG_EDITION_STATEMENT = 'editionStmt'
-TAG_PUB_STATEMENT = 'publicationStmt'
-TAG_DATE = 'date'
-TAG_DISTRIBUTOR = 'distributor'
-TAG_LICENSE = 'availability'
-TAG_TEXT = 'text'
-TAG_FRONT = 'front'
-TAG_BODY = 'body'
-
 
 class TEIReader(object):
-
-    query_template = '//{namespace}tag'
 
     def __init__(self, file_name):
         self.file_name = file_name
@@ -33,16 +17,6 @@ class TEIReader(object):
         namespace = root_tag[:ns_index]
 
         doc = tei_document.TEIDocument(tree, namespace)
-
-        header = etree.find(TAG_HEADER)
-        file_desc = header.find(TAG_FILE_DESC)
-        title = file_desc.find(TAG_TITLE)
-        principal = file_desc.find(TAG_PRINCIPAL)
-        edition = file_desc.find(TAG_EDITION_STATEMENT)
-        publication = file_desc.find(TAG_PUB_STATEMENT)
-        date = publication.find(TAG_DATE)
-        availability = file_desc.find(TAG_LICENSE)
-        text = doc.find(TAG_TEXT)
-        front = text.find(TAG_FRONT)
-        body = text.find(TAG_BODY)
+        metadata = doc.get_metadata()
+        return metadata
 
--- preprocessing/tei/tei_document.py
+++ preprocessing/tei/tei_document.py
@@ -1,13 +1,43 @@
-from lxml import etree
+TAG_HEADER = 'teiHeader'
+TAG_FILE_DESC = 'fileDesc'
+TAG_TITLE_STATEMENT = 'titleStmt'
+TAG_TITLE = 'title'
+TAG_PRINCIPAL = 'principal'
+TAG_EDITION_STATEMENT = 'editionStmt'
+TAG_PUB_STATEMENT = 'publicationStmt'
+TAG_DATE = 'date'
+TAG_DISTRIBUTOR = 'distributor'
+TAG_LICENSE = 'availability'
+TAG_TEXT = 'text'
+TAG_FRONT = 'front'
+TAG_BODY = 'body'
 
 
 class TEIDocument(object):
+
+    query_template = '//{namespace}{tag}'
 
     def __init__(self, tree, namespace):
         self.tree = tree
         self.namespace = namespace
 
-    def get_element(self, tree, tag, namespace):
+    def get_metadata(self):
+        return {'title': self.get_element_text(TAG_TITLE),
+                'edition': self.get_element_text(TAG_EDITION_STATEMENT),
+                'date': self.get_element_text(TAG_DATE),
+                'availability': self.get_element_text(TAG_LICENSE),
+                'body': self.get_element_text(TAG_BODY),
+        }
+
+    def get_element_text(self, tag):
+        # TODO: go into p and div to get text
+        element = self.get_element(tag)
+        if element is not None:
+            return element.text
+        else:
+            return 'No ' + tag
+
+    def get_element(self, tag):
         """
 
         :param tree:
@@ -15,14 +45,14 @@ class TEIDocument(object):
         :param namespace:
         :return:
         """
-        query = self.get_tag_query(namespace, tag)
-        return tree.find(query)
+        query = self.get_tag_query(tag)
+        return self.tree.find(query)
 
-    def get_tag_query(self, namespace, tag):
-        return self.query_template.format(namespace=namespace,
+    def get_tag_query(self, tag):
+        return self.query_template.format(namespace=self.namespace,
                                           tag=tag)
 
-    def get_elements(self, tree, tag, namespace):
+    def get_elements(self, tag):
         """
         Return all elements of tag
         :param tree:
@@ -30,5 +60,5 @@ class TEIDocument(object):
         :param namespace:
         :return:
         """
-        query = self.get_tag_query(namespace, tag)
-        return tree.findall(query)
+        query = self.get_tag_query(tag)
+        return self.tree.findall(query)
